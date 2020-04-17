cdb7592f6c62a97bdb13ec32977611f015f680d2
hughesbuildingcompany <63839162+hughesbuildingcompany@users.noreply.github.com>
2020-04-17 06:33:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// JS FOR CONTACT-FORM
+
+//const firstName = document.getElementById('first-name');
+//const lastName = document.getElementById('lastName');
+//const email = document.getElementById('email');
+//const yourCity = document.getElementById('your-city');
+//const phoneNumber = document.getElementById('phone-number');
+//const postalCode = document.getElementById('postal-code');
+//let emailCheckbox = document.getElementById('email-preference');
+//let phoneCheckbox = document.getElementById('phone-preference');
+//
+//form.addEventListener('submit', e => {
+//  let messages = [];
+//
+//  if (firstName.value === '' || firstName.value === null) {
+//    messages.push('Your first name is required please');
+//  }
+//
+//  if (lastName.value === '' || lastName.value === null) {
+//    messages.push('Fill out your last name too!');
+//  }
+//
+//  if (messages.length > 0) {
+//    e.preventDefault();
+//  }
+//});
